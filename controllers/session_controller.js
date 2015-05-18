7c2b5396c440a11cd9126d1d91ba1404ee70be42
gpedregosa <pedregosa94@gmail.com>
2015-05-18 17:19:28 +0200

    AutoLogout

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -13,10 +13,13 @@ var errors = req.session.errors || {};
 req.session.errors = {};
 res.render('sessions/new', {errors: errors});
 };
+
 // POST /login -- Crear la sesion si usuario se autentica
 exports.create = function(req, res) {
 var login = req.body.login;
 var password = req.body.password;
+var time = new Date();
+var timeLogin = time.getTime();
 var userController = require('./user_controller');
 userController.autenticar(login, password, function(error, user) {
 if (error) { // si hay error retornamos mensajes de error de sesión
@@ -26,12 +29,33 @@ return;
 }
 // Crear req.session.user y guardar campos id y username
 // La sesión se define por la existencia de: req.session.user
-req.session.user = {id:user.id, username:user.username};
+req.session.user = {id:user.id, username:user.username, timeLogin: timeLogin};
 res.redirect(req.session.redir.toString());// redirección a path anterior a login
 });
 };
+
 // DELETE /logout -- Destruir sesion
 exports.destroy = function(req, res) {
 delete req.session.user;
 res.redirect(req.session.redir.toString()); // redirect a path anterior a login
+};
+
+//autologout
+exports.autoLogout = function(req, res, next){
+var logDate = new Date();
+var date = logDate.getTime();
+if(!req.session.user){
+next();
+} else{
+var difference = date - req.session.user.timeLogin;
+console.log(difference);
+req.session.user.timeLogin=date;
+if(difference<120000){
+next();
+}
+else{
+delete req.session.user;
+res.redirect('/');
+}
+}
 };
--- routes/index.js
+++ routes/index.js
@@ -15,26 +15,26 @@ router.param('quizId', quizController.load); // autoload:quizId
 router.param('commentId', commentController.load); // autoload :commentId
 
 // Definición de rutas de sesion
-router.get('/login', sessionController.new); // formulario login
-router.post('/login', sessionController.create); // crear sesión
-router.get('/logout', sessionController.destroy); // destruir sesión
+router.get('/login', sessionController.autoLogout, sessionController.new); // formulario login
+router.post('/login', sessionController.autoLogout, sessionController.create); // crear sesión
+router.get('/logout', sessionController.autoLogout, sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
-router.get('/quizes', quizController.index);
-router.get('/quizes/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes', sessionController.autoLogout, quizController.index);
+router.get('/quizes/quizes', sessionController.autoLogout, quizController.index);
+router.get('/quizes/:quizId(\\d+)', sessionController.autoLogout, quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', sessionController.autoLogout, quizController.answer);
 
-router.get('/quizes/new', sessionController.loginRequired, quizController.new);
-router.post('/quizes/create', sessionController.loginRequired, quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
-router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
-router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
+router.get('/quizes/new', sessionController.autoLogout, sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.autoLogout, sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.autoLogout, sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.autoLogout, sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.autoLogout, sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
+router.get('/quizes/:quizId(\\d+)/comments/new', sessionController.autoLogout, commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', sessionController.autoLogout, commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.autoLogout, sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res) {
     res.render('author', { title: 'Créditos', errors: [] });
