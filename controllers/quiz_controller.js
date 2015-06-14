2d6499289e5d2a38cb53d3cdaba1b1705c407a65
gpedregosa <pedregosa94@gmail.com>
2015-06-14 19:40:48 +0200

    Añadidas categorias de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,108 +1,113 @@
 var models = require('../models/models.js');
 // Autoload :id
 exports.load = function(req, res, next, quizId) {
-models.Quiz.find({
-where: {
-id: Number(quizId)
-},
-include: [{
-model: models.Comment
-}]
-}).then(
-function(quiz) {
-if (quiz) {
-req.quiz = quiz;
-next();
-} else{next(new Error('No existe quizId=' + quizId))}
-}
-).catch(function(error){next(error)});
+    models.Quiz.find({
+        where: {
+            id: Number(quizId)
+        },
+        include: [{
+            model: models.Comment
+        }]
+    }).then(
+        function(quiz) {
+            if (quiz) {
+                req.quiz = quiz;
+                next();
+            } else{next(new Error('No existe quizId=' + quizId))}
+        }
+    ).catch(function(error){next(error)});
 };
+
 // GET /quizes
 exports.index = function(req, res) {
-var query = req.query.search;
-if(query){
-console.log("buscando...");
-models.Quiz.findAll({where:["pregunta like ?", '%' + query + '%']}).then(function(quizes) {
-res.render('quizes', {quizes: quizes, errors: []});
-})
-} else {
-console.log("nada que buscar");
-models.Quiz.findAll().then(
-function(quizes) {
-res.render('quizes/index', {quizes: quizes, errors: []});
-}).catch(function(error){next(error);})
-}
+    var query = req.query.search;
+    if(query){
+        console.log("buscando...");
+        models.Quiz.findAll({where:["pregunta like ?", '%' + query + '%']}).then(function(quizes) {
+            res.render('quizes', {quizes: quizes, errors: []});
+        })
+    } else {
+        console.log("nada que buscar");
+        models.Quiz.findAll().then(
+            function(quizes) {
+                res.render('quizes/index', {quizes: quizes, errors: []});
+            }).catch(function(error){next(error);})
+    }
 };
+
 // GET /quizes/:id
 exports.show = function(req, res) {
-res.render('quizes/show', { quiz: req.quiz, errors: []});
+    res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
+
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-var resultado = 'Incorrecto';
-if (req.query.respuesta === req.quiz.respuesta) {
-resultado = 'Correcto';
-}
-res.render(
-'quizes/answer',
-{ quiz: req.quiz,
-respuesta: resultado,
-errors: []
-}
-);
+    var resultado = 'Incorrecto';
+    if (req.query.respuesta === req.quiz.respuesta) {
+        resultado = 'Correcto';
+    }
+    res.render(
+        'quizes/answer',
+        { quiz: req.quiz,
+         respuesta: resultado,
+         errors: []
+        }
+    );
 };
+
 // GET /quizes/new
 exports.new = function(req, res) {
-var quiz = models.Quiz.build( // crea objeto quiz
-{pregunta: "Pregunta", respuesta: "Respuesta"}
-);
-res.render('quizes/new', {quiz: quiz, errors: []});
+    var quiz = models.Quiz.build( // crea objeto quiz
+        {pregunta: "Pregunta", respuesta: "Respuesta", categoria: "Categoría"}
+    );
+    res.render('quizes/new', {quiz: quiz, errors: []});
 };
+
 // POST /quizes/create
 exports.create = function(req, res) {
-var quiz = models.Quiz.build( req.body.quiz );
-quiz
-.validate()
-.then(
-function(err){
-if (err) {
-res.render('quizes/new', {quiz: quiz, errors: err.errors});
-} else {
-quiz // save: guarda en DB campos pregunta y respuesta de quiz
-.save({fields: ["pregunta", "respuesta"]})
+    var quiz = models.Quiz.build( req.body.quiz );
+    quiz.validate().then(
+        function(err){
+            if (err) {
+                res.render('quizes/new', {quiz: quiz, errors: err.errors});
+            } else {
+                quiz // save: guarda en DB campos pregunta y respuesta de quiz
+.save({fields: ["pregunta", "respuesta", "categoria"]})
 .then( function(){ res.redirect('/quizes')})
 } // res.redirect: Redirección HTTP a lista de preguntas
 }
 ).catch(function(error) {next(error)});
 };
+
 // GET /quizes/:id/edit
 exports.edit = function(req, res) {
-var quiz = req.quiz; // req.quiz: autoload de instancia de quiz
-res.render('quizes/edit', {quiz: quiz, errors: []});
+    var quiz = req.quiz; // req.quiz: autoload de instancia de quiz
+    res.render('quizes/edit', {quiz: quiz, errors: []});
 };
+
 // PUT /quizes/:id
 exports.update = function(req, res) {
-req.quiz.pregunta = req.body.quiz.pregunta;
-req.quiz.respuesta = req.body.quiz.respuesta;
-req.quiz
-.validate()
-.then(
-function(err){
-if (err) {
-res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
-} else {
-req.quiz // save: guarda campos pregunta y respuesta en DB
-.save( {fields: ["pregunta", "respuesta"]})
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.categoia = req.body.quiz.categoria;
+    req.quiz.validate().then(
+        function(err){
+            if (err) {
+                res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+            } else {
+                req.quiz // save: guarda campos pregunta y respuesta en DB
+.save( {fields: ["pregunta", "respuesta", "categoria"]})
 .then( function(){ res.redirect('/quizes');});
 } // Redirección HTTP a lista de preguntas (URL relativo)
 }
 ).catch(function(error) {next(error)});
 };
+
 // DELETE /quizes/:id
 exports.destroy = function(req, res) {
-req.quiz.destroy().then(function(){
-res.redirect('/quizes');
-}).catch(function(error) {next(error)});
+    req.quiz.destroy().then(function(){
+        res.redirect('/quizes');
+    }).catch(function(error) {next(error)});
 };
 
 // GET /quizes/statistics
--- models/quiz.js
+++ models/quiz.js
@@ -1,12 +1,16 @@
 // Definicion del modelo de Quiz con validación
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('Quiz',{ pregunta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
-    },
-    respuesta: {type: DataTypes.STRING,
-validate: { notEmpty: {msg: "-> Falta Respuesta"}}
-               }
+    return sequelize.define('Quiz',{
+        pregunta: {
+            type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+        },
+        respuesta: {type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+        },
+        categoria: {type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Categoría"}}
+        }
 }
 );
 }
