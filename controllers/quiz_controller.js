4e8ac3cf7ea6941a30134651c3842faf5e4791b9
gpedregosa <pedregosa94@gmail.com>
2015-06-15 10:20:54 +0200

    Fix quiz_controller

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -89,7 +89,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
     req.quiz.pregunta = req.body.quiz.pregunta;
     req.quiz.respuesta = req.body.quiz.respuesta;
-    req.quiz.categoia = req.body.quiz.categoria;
+    req.quiz.categoria = req.body.quiz.categoria;
     req.quiz.validate().then(
         function(err){
             if (err) {
